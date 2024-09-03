b7bb4f9811a8d21b13bda46dc8b7137ce081712c
Dhanushka-Sasanka <dhanushkasasankapayza@gmail.com>
2024-09-03 23:37:44 +0530

    OrderController Updated.

--- src/main/java/com/idet/simple_api/controller/OrderController.java
+++ src/main/java/com/idet/simple_api/controller/OrderController.java
@@ -1,9 +1,20 @@
 package com.idet.simple_api.controller;
 
+import com.idet.simple_api.entity.Item;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/order/")
 public class OrderController {
+
+    @PostMapping()
+    public ResponseEntity<Item> createItem(@RequestBody Item item) {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+
+    }
 }
